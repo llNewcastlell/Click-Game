833ce67b64d1e2b479c9a7d3663cb2712d507e5b
Newcastle <vaughnmarques@gmail.com>
2018-10-09 18:15:30 -0600

    Updates

---
+++ precache-manifest.8b20bb9daf68704527f2d92eca1af284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8390b4b4958d34b3871a",
+    "url": "/static/js/main.8390b4b4.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "8390b4b4958d34b3871a",
+    "url": "/static/css/main.495112ff.chunk.css"
+  },
+  {
+    "revision": "5566a62bb4025f832d4f18369d0b7b28",
+    "url": "/index.html"
+  }
+];
